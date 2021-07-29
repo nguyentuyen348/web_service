e2ce66acc1d938309eace95c071be917d44d1dee
nguyentuyen348 <nguyentuyen348@gmail.com>
2021-07-29 08:42:36 +0700

    web service

---
+++ app/Services/CustomerServiceImpl.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\Services;
+
+interface CustomerServiceImpl
+{
+    public function getAll();
+
+    public function findById($id);
+
+    public function create($request);
+
+    public function update($request, $id);
+
+    public function destroy($id);
+}
